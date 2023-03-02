19acc82b89f85fee49a708b4262dc7a075e3b206
Mathis Girault <mathis.girault38@gmail.com>
2023-03-02 16:26:33 +0100

    Merge avec Jules et modif de minmax

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,18 @@ extern crate blobwar;
 //use blobwar::board::Board;
 use blobwar::configuration::Configuration;
 use blobwar::strategy::{Greedy, Human, MinMax};
+use std::time::{Instant};
 
 fn main() {
-    //let board = Board::load("x").expect("failed loading board");
+    let start = Instant::now();
+
+    // let board = Board::load("x").expect("failed loading board");
     let board = Default::default();
     let mut game = Configuration::new(&board);
     // game.battle(MinMax(1), Greedy());
-    //game.battle(Greedy(), MinMax(1));
-    game.battle(MinMax(1), MinMax(1));
+    // game.battle(Greedy(), MinMax(1));
+    game.battle(MinMax(1), MinMax(3));
+
+    let duration = start.elapsed();
+    println!("Time elapsed in expensive_function() is: {:?}", duration);
 }
--- src/strategy/minmax.rs
+++ src/strategy/minmax.rs
@@ -6,52 +6,56 @@ use std::fmt;
 /// Min-Max algorithm with a given recursion depth.
 pub struct MinMax(pub u8);
 
-fn search_next_move(reel_depth: u8, state: &Configuration, depth: u8, player: bool) -> (i8, Option<Movement>) {
-    if depth == 0 {
-        //print!("Deapth vaut zéro\n");
-        return ((-1 as i8).pow(reel_depth as u32)*state.value(), None);
-    }
-    let mut value = if player {i8::MIN} else {i8::MAX};
-    let mut next_mov = None;
-
-    if player {
-        for movement in state.movements() {
-            let new_state:Configuration = state.play(&movement);
-            //print!("mov avant recherche récursive mov: {movement:?}\n");
-            let new_value = search_next_move(reel_depth, &new_state, depth - 1, false);
-            //print!("best_mov: {next_mov:?} mov: {movement:?}, best_val: {value} val: {}\n", new_value.0);
-            if new_value.0 > value {
-                value = new_value.0;
-                next_mov = Some(movement);
-            }
-            // print!("best_mov: {next_mov:?} next mov: {movement:?}, best_val: {value} val: {}\n", new_value.0);
-        }
-    }
-    else {
-        for movement in state.movements() {
-            let new_state:Configuration = state.play(&movement);
-            // print!("mov avant recherche récursive mov: {movement:?}\n");
-            let new_value = search_next_move( reel_depth, &new_state, depth - 1, true);
-            // print!("best_mov: {next_mov:?} next mov: {movement:?}, best_val: {value} val: {}\n", new_value.0);
-            if new_value.0 < value {
-                value = new_value.0;
-                next_mov = Some(movement);
-            }
-        }
-    };
-
-    return (value, next_mov);
-}
-
 
 impl Strategy for MinMax {
     fn compute_next_move(&mut self, state: &Configuration) -> Option<Movement> {
         
-        let next_mov = search_next_move(self.0, state, self.0 + 1, true).1;
-        //print!("{next_mov:?}");
-        return next_mov;
+        let mut best_move: Option<Movement> = None;
+        let mut best_score = i8::MIN;
+
+        for movement in state.movements() {
+            let next_state = state.play(&movement);
+            let score = min_max(&next_state, self.0, self.0, false);
+            
+            if score > best_score {
+                best_score = score;
+                best_move = Some(movement);
+            }
+        }
+        
+        best_move
     }
 }
+
+
+fn min_max(state: &Configuration, real_depth: u8, depth: u8, max_player: bool) -> i8 {
+    
+    if depth == 0 {
+        return (-1 as i8).pow(real_depth as u32)*state.value();
+    }
+
+    let mut best_score = if max_player {i8::MIN} else {i8::MAX};
+
+    
+    if max_player {
+        for movement in state.movements() {
+            let next_state = state.play(&movement);
+            let score = min_max(&next_state, real_depth, depth - 1, !max_player);
+            best_score = std::cmp::max(best_score, score);
+        }
+
+    } else {
+        for movement in state.movements() {
+            let next_state = state.play(&movement);
+            let score = min_max(&next_state, real_depth, depth - 1, !max_player);
+            best_score = std::cmp::min(best_score, score);
+        }
+    }
+
+
+    best_score
+}
+
 
 impl fmt::Display for MinMax {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
